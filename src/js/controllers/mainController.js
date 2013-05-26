6cb2b69e6ae50bcace8f023ae69be643b789388f
Ivar Nilsen <ivar.nilsen@gmail.com>
2013-05-26 22:05:56 +0200

    Looking at topcoat.io

--- src/js/controllers/mainController.js
+++ src/js/controllers/mainController.js
@@ -6,15 +6,11 @@
             { text: 'Exercises', url: '/exercises' },
             { text: 'Planner' , url: '/planner'}
         ];
-        var selected = selected || $scope.choices[0];
+        $scope.selected = $scope.choices.filter(function(c) { 
+            return c.url === $location.url();
+        })[0] || $scope.choices[0];
 
-        $scope.navigate = function(choice) {
-            selected = choice;
-        }
-
-        $scope.active = function (choice) {
-            return $location.path() === choice.url ? 'active' : '';
-        }
+        $scope.showNavigation = false;
 
     }]);
 })();
